3196bc800bf04e29e76586ec80bc035bcb490ee0
foreverxiongtao <foreverxiongtao@gmail.com>
2019-04-29 10:18:55 +0800

    描述:  引入mockito测试框架，人员，保存，删除，跳过功能单元测试 影响范围(模块): exercise3 测试建议:建议测试 自测通过:自测通过 风险(可选): reviewed by:xt

--- exercise3/app/src/androidTest/java/com/example/meeting/PersonTest.java
+++ exercise3/app/src/androidTest/java/com/example/meeting/PersonTest.java
@@ -4,28 +4,18 @@ package com.example.meeting;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
-import com.example.library.helper.RxHelper;
-import com.example.library.utils.LogUtils;
 import com.example.meeting.constant.GlobalConstant;
 import com.example.meeting.db.AppDatabase;
 import com.example.meeting.model.entity.User;
-import io.reactivex.Maybe;
-import io.reactivex.Observable;
 import io.reactivex.Scheduler;
 import io.reactivex.android.plugins.RxAndroidPlugins;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subscribers.TestSubscriber;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-
-import java.util.List;
 
 /**
  * author : desperado
@@ -35,7 +25,7 @@ import java.util.List;
  * version: 1.0
  */
 @RunWith(AndroidJUnit4.class)
-public class PersonaTest {
+public class PersonTest {
 
 
     @Before
@@ -51,6 +41,9 @@ public class PersonaTest {
     }
 
 
+    /***
+     * 初始化rxJava
+     */
     private void initRxJava() {
         RxJavaPlugins.reset();
         RxJavaPlugins.setIoSchedulerHandler(new Function<Scheduler, Scheduler>() {
@@ -74,28 +67,68 @@ public class PersonaTest {
      */
     @Test
     public void saveUser() {
-        final User user = Mockito.spy(User.class);
-        Mockito.when(user.getName()).thenReturn("xiongtao");
-        Mockito.when(user.getNo()).thenReturn(1);
-        Mockito.when(user.getCreateTime()).thenReturn(System.currentTimeMillis());
-        Mockito.when(user.getIsDelete()).thenReturn(GlobalConstant.VALUE_IS_NOT_DELETE);
-        Mockito.when(user.getIsSkip()).thenReturn(GlobalConstant.VALUE_IS_NOT_SKIP);
+        User user = createUser("xiongtao", 1);
         //保存人员到数据库
         Long id = AppDatabase.getInstance().userDao().insertUsers(user);
-        User targetUser = AppDatabase.getInstance().userDao().getUserByNo(user.getNo());
-        assert user.getName() == targetUser.getName();
         //再根据刚才插入的id去数据库查询
+        User targetUser = AppDatabase.getInstance().userDao().getUserByNo(user.getNo());
+        //相等则保存成功
+        assert user.getName() == targetUser.getName();
     }
 
 
+    /***
+     * 创建用户
+     * @param name
+     * @param no
+     * @return
+     */
+    public User createUser(String name, int no) {
+        final User user = Mockito.mock(User.class);
+        Mockito.when(user.getName()).thenReturn(name);
+        Mockito.when(user.getNo()).thenReturn(no);
+        Mockito.when(user.getCreateTime()).thenReturn(System.currentTimeMillis());
+        Mockito.when(user.getIsDelete()).thenReturn(GlobalConstant.VALUE_IS_NOT_DELETE);
+        Mockito.when(user.getIsSkip()).thenReturn(GlobalConstant.VALUE_IS_NOT_SKIP);
+        return user;
+    }
+
+
+    /***
+     * 测试用户删除
+     */
     @Test
     public void deletePerson() {
-//        initRxJava();
-          User user = AppDatabase.getInstance().userDao().getNewestNumber2();
-//        TestSubscriber<User> subscriber = TestSubscriber.create();
-//        obserable.subscribe((Consumer<? super User>) subscriber);
-//        subscriber.assertNoErrors();
-//        subscriber.assertComplete();
-//        List<Object> objects = subscriber.getEvents().get(0);
+        User user = createUser("xiongtao", 1);
+        //保存用户到数据库
+        long row1 = AppDatabase.getInstance().userDao().insertUsers(user);
+        assert row1 > 0;
+        Mockito.when(user.getId()).thenReturn((int) row1);  //设置刚才的用户id
+        //设置当前用户的删除状态为删除
+        Mockito.when(user.getIsDelete()).thenReturn(GlobalConstant.VALUE_IS_DELETE);
+        //修改数据库的删除状态
+        row1 = AppDatabase.getInstance().userDao().updateUser(user);
+        assert row1 > 0;
+        User targetUser = AppDatabase.getInstance().userDao().getUserByNo(user.getNo());
+
+        assert targetUser.getIsDelete() == user.getIsDelete();
+    }
+
+
+    /***
+     * 跳过某一个人功能
+     */
+    @Test
+    public void skip() {
+        User user = createUser("xiongtao", 1);
+        //保存用户到数据库
+        long row1 = AppDatabase.getInstance().userDao().insertUsers(user);
+        assert row1 > 0;
+        Mockito.when(user.getId()).thenReturn((int) row1);  //设置刚才的用户id
+        Mockito.when(user.getIsSkip()).thenReturn(GlobalConstant.VALUE_IS_SKIP); //设置当前的用户为已跳过
+        row1 = AppDatabase.getInstance().userDao().updateUser(user);
+        assert row1 > 0;
+        User targetUser = AppDatabase.getInstance().userDao().getUserByNo(user.getNo());
+        assert targetUser.getIsSkip() == user.getIsSkip();
     }
 }
